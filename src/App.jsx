7b4159b839d52cad7a976c4b3e8f2950cbbbfd46
Aditeeniraula <aditeeniraula@gmail.com>
2025-02-20 15:36:51 +0545

    mobile view

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import {
+  useNavigate,
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
 import AdminLogin from "./pages/admin/AdminLogin";
 import AdminDashboard from "./pages/admin/AdminDashboard";
 import Compare from "./components/Compare";
@@ -6,23 +12,35 @@ import TableView from "./components/TableView";
 import CardView from "./components/CardView";
 import FamilyTreeGraph from "./components/FamilyTreeGraph";
 
+const RedirectOnMobile = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (window.innerWidth < 768) {
+      navigate("/card");
+    }
+  }, [navigate]);
+  return null;
+};
+
+const AppRoutes = () => (
+  <>
+    <RedirectOnMobile />
+    <Routes>
+      <Route path="/login" element={<AdminLogin />} />
+      <Route path="/admin" element={<AdminDashboard />} />
+      <Route path="/card/:id" element={<CardView />} />
+      <Route path="/compare/:id" element={<Compare />} />
+      <Route path="/" element={<TableView />} />
+      <Route path="/:id" element={<TableView />} />
+      <Route path="/card" element={<CardView />} />
+    </Routes>
+  </>
+);
+
 const App = () => {
   return (
     <Router basename="/Banshali-app">
-      <Routes>
-        {/* Uncomment these if needed */}
-        <Route path="/login" element={<AdminLogin />} />
-        <Route path="/admin" element={<AdminDashboard />} />
-        {/* <Route path="card/:id" element={<AdminDashboard />} /> */}
-        <Route path="card/:id" element={<CardView />} />
-
-        <Route path="/compare/:id" element={<Compare />} />
-        <Route path="/" element={<TableView />} />
-        {/* <Route path="/familytree/:id" element={<FamilyTreeGraph />} /> */}
-        <Route path="/:id" element={<TableView />} />
-
-        <Route path="/card" element={<CardView />} />
-      </Routes>
+      <AppRoutes />
     </Router>
   );
 };
