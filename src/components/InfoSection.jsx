deee1b3e39a8ee884fe4822fc94aac5e698105ae
Aditeeniraula <aditeeniraula@gmail.com>
2025-05-02 20:02:24 +0545

    CSS

--- src/components/InfoSection.jsx
+++ src/components/InfoSection.jsx
@@ -37,7 +37,7 @@ const InfoSection = ({ person }) => {
             display: flex;
             flex-direction: column;
             gap: 24px;
-            padding-bottom: 15px;
+            padding-bottom: 25px;
           }
 
           .info-box {
