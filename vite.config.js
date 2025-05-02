df75df6aff5e9bc73544e5e2e6e4d0f1a4ee0df4
SwopnilBista <swopnilbista2000@gmail.com>
2025-05-02 21:32:31 +0545

    Remove HMR configuration from Vite server settings

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     host: '0.0.0.0',  // Allows external access
     port: 5173,       // Ensure this port is open
     allowedHosts: ['gautamfamily.org.np',
       "http://54.173.191.197",],
-    hmr: {
-      protocol: 'wss',
-      host: 'gautamfamily.org.np',
-    },
+   
   },
 })
